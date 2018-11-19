27f7cbf3843b59b9779ed9b1cf92be342e031d1f
grosclara <gros.clara@hotmail.fr>
2018-11-19 17:02:47 +0100

    lk

--- MVP 1/Analyse_Globale.py
+++ MVP 1/Analyse_Globale.py
@@ -11,15 +11,6 @@ def data(filepath):
     :param filepath : fichier ruby à analyser
     :return dict : dictionnaire contenant le critère et sa valeur"""
     dict={}
-<<<<<<< HEAD
-    dict['Nombrefonctions']=count_functions(filepath)
-    dict['Nombretests']=count_tests(filepath)
-    dict['Nombrecommentaires']=commentaires(filepath)
-    dict['Nombrevariables']=count_variables(filepath)
-    dict['Taillefonctions']=function_size(filepath)
-    dict['Nombrelignes']=num_lines
-    return dict
-=======
     dict['Nombre fonctions']=count_functions(filepath)
     dict['Nombre tests']=count_tests(filepath)
     dict['Nombre commentaires']=commentaires(filepath)
@@ -27,7 +18,6 @@ def data(filepath):
     dict['Taille fonctions']=function_size(filepath)
     return(dict)
     print(dict)
->>>>>>> brancheClara
 
 data('C:/Users/Gros/PycharmProjects/Lequipe/EventCandidatA.rb')
 
