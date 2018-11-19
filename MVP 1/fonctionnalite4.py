d66a6899f28ae3b5b32442e96d2ee4abe8a7cbb3
marine.wigniolle@student-cs.fr <Benodet29!>
2018-11-19 16:46:46 +0100

    Fonctionnalité 4 Marine

---
+++ MVP 1/fonctionnalite4.py
@@ -0,0 +1,27 @@
+from Fonctionnalite2_5 import *
+
+def note_functions(dict,filepath):
+    if 4<=dict['Nombrefonctions']<=6:
+        note=10
+        for k in range(len(function_size(filepath)[0])):
+            if not 4<=function_size(filepath)[0][k]<=8:
+                note-=0,5
+        if note <0:
+            return 0
+    if 2<=dict['Nombrefonctions']<4 or 6<dict['Nombrefonctions']<=8:
+        note=8
+        for k in range(len(function_size(filepath)[0])):
+            if not 4<=function_size(filepath)[0][k]<=8:
+                note-=0,5
+        if note <0:
+            return 0
+    if 0<=dict['Nombrefonctions']<2 or 8<dict['Nombrefonctions']:
+        note=5
+        for k in range(len(function_size(filepath)[0])):
+            if not 4<=function_size(filepath)[0][k]<=8:
+                note-=0,5
+        if note <0:
+            return 0
+
+
+
