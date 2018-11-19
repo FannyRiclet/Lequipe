a9da8cb768a9dd923f322491fcec82daba98466b
grosclara <gros.clara@hotmail.fr>
2018-11-19 11:43:58 +0100

    comptage fonctions

---
+++ MVP 1/Fct2_1.py
@@ -0,0 +1,12 @@
+def count_functions(filepath) :
+    with open(filepath,'r') as file :
+        lines=file.readlines()
+        count=0
+        print(lines)
+        for line in lines :
+            print(line)
+            if "def" in line :
+                count+=1
+    print("Le code contient {} fonctions".format(count))
+
+filepath='C:/Users/Gros/PycharmProjects/Lequipe/EventCandidatA.rb'
