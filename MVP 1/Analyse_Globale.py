0d164cca54414a77981d8f81dfcb79fcc03f2008
marine.wigniolle@student-cs.fr <Benodet29!>
2018-11-21 11:13:56 +0100

    Ajout MVP2/Fonctionnalité2_3

--- MVP 1/Analyse_Globale.py
+++ MVP 1/Analyse_Globale.py
@@ -1,26 +1,20 @@
-from Fonctionnalite2_1 import *
-from Fonctionnalite2_2 import *
-from Fonctionnalite2_3 import *
-from Fonctionnalite2_4 import *
-from Fonctionnalite2_5 import *
+import *
 
 def data(filepath):
     """Renvoie les données brutes des critères pour analyser un fichier Ruby d'un candidat
     :param filepath : fichier ruby à analyser
     :return dict : dictionnaire contenant le critère et sa valeur"""
     dict={}
-    dict['Nombre fonctions']=count_functions(filepath)
-    dict['Nombre tests']=count_tests(filepath)
-    dict['Nombre commentaires']=commentaires(filepath)
-    dict['Nombre variables']=count_variables(filepath)
-    dict['Taille fonctions']=function_size(filepath)
+    dict['Nombrefonctions']=count_functions(filepath)
+    dict['Nombretests']=count_tests(filepath)
+    dict['Nombrecommentaires']=commentaires(filepath)
+    dict['Nombrevariables']=count_variables(filepath)
+    dict['Taillefonctions']=function_size(filepath)
     print(dict)
     return(dict)
 
-<<<<<<< HEAD
-=======
-
-data('C:/Users/Gros/PycharmProjects/Lequipe/EventCandidatA.rb')
 
 
->>>>>>> ba5cc7880fb6ba5a1b73f4931053871818d485e2
+data('/Users/marinewigniolle/PycharmProjects/Codingweek_semaine2/Lequipe/EventCandidatA.rb')
+
+
