30fd8df4650a507a63faa3c65bb4a71bbe3b9ea8
Lison <lison.lebiavant@student-cs.fr>
2018-11-21 11:27:22 +0100

    rectf mvp 2 fct 2

--- MVP 2/Fonctionnalite2_2.py
+++ MVP 2/Fonctionnalite2_2.py
@@ -4,15 +4,11 @@ def longueur_nom_variables(filepath):
     with open(filepath,'r') as file :
         lines=file.readlines()
         for line in lines :
-            if "=" in line :
-                position=line.find("=")
+            if " = " in line :
+                position=line.find(" = ")
                 count=0 #compte le nb de caracteres d'un nom de variable
-                i=position-2 #indice qui parcourt la ligne, commence deux indices avant le '='
+                i=position-1 #indice qui parcourt la ligne, commence deux indices avant le ' = '
                 while i>=0 and line[i]!=' ':
                     count+=1
                     i-=1 #on rebrousse chemin
                 print(count)
-
-filepath='C:/Users/Arnaud et Lydie/Desktop/Lison/CS/SIP/Coding_weeks/Doctolib/Lequipe/EventCandidatA.rb'
-
-
