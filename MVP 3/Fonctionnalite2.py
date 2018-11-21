fdded9407ad4d49a2432965820b8d51d73f4b7ac
marine.wigniolle@student-cs.fr <Benodet29!>
2018-11-21 16:08:23 +0100

    Fonctionnalite 2 MVP 3

---
+++ MVP 3/Fonctionnalite2.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('../MVP 4 ')
+from Tableau_recapitulatif import *
+
+
+def comparaison_deux_candidats(filepath1,filepathtest1, filepath2, filepathtest2,liste_critere):
+    """Compare un fichier  d'un candidats sur les criteres determines en mvp1 et mvp2
+    :param filepath1: fichier ruby du candidat1
+    :param filepathtest1 : fichier test ruby du candidat1
+    :param filepath2: fichier ruby du candidat2
+    :param filepathtest2 : fichier test ruby du candidat2
+    :param liste_critere : liste des criteres de comparaison
+    :return: Un dictionnaire contenant le nom du critere et le taux de ressemblance pour ce critere
+    """
+    dict1=data_finale(filepath1,filepathtest1)
+    dict2=data_finale(filepath2, filepathtest2)
+    dico={}
+    for x in liste_critere :
+        dico[x]=min(dict1[x],dict2[x])/max(dict1[x],dict2[x])*100
+
+    return dico
+
+
+
+
---
+++ MVP 3/Test.py
@@ -0,0 +1,8 @@
+L=['coucou', 'yo']
+d={}
+k=0
+for x in L:
+    d[x]=k
+    k+=1
+
+print d
