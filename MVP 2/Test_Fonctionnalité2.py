4f968b3f31ef340915381c0bf4e3379bc149c36f
Camille <you@example.com>
2018-11-22 14:33:59 +0100

    amélioration test

--- MVP 2/Test_Fonctionnalité2.py
+++ MVP 2/Test_Fonctionnalité2.py
@@ -5,26 +5,28 @@ from Fonctionnalite2_2_2 import *
 from Fonctionnalite2_2_3 import *
 from Fonctionnalite2_2_4 import *
 from Fonctionnalite2_2_5 import *
+def les_filepath(Nom_dev,Nom_Candidat): #nom du developpeur et Nom_Candidat chaines de caractères
+    return(['C:/Users/{}/PycharmProjects/Lequipe/Event{}.rb'.format(Nom_dev,Nom_Candidat),'C:/Users/{}/PycharmProjects/Lequipe/Event{}Test.rb'.format(Nom_dev,Nom_Candidat)])
 
-def test_longueur_nom_fonctions(emplacement_projet):
+def test_longueur_nom_fonctions(Nom_dev,Nom_Candidat):
     """teste la fonction longueur_nom_fonctions
-    :param: l'emplacement du projet pour completer le filepath"""
-    assert longueur_nom_fonctions(emplacement_projet + "Lequipe\EventCandidatA.rb") == [[8,12,19,40,48,42,48,46,20],21]
+    :param: le nom du développeur et le nom du candidat dont on analyse le code"""
+    assert longueur_nom_fonctions(les_filepath(Nom_dev,Nom_Candidat)[0]) == [[8,12,19,40,48,42,48,46,20],21]
 
-def test_longueur_nom_variables(emplacement_projet):
+def test_longueur_nom_variables(Nom_dev,Nom_Candidat):
     """teste la fonction longueur_nom_varaiables
-    :param: l'emplacement du projet pour completer le filepath"""
-    assert longueur_nom_variables(emplacement_projet + "Lequipe\EventCandidatA.rb") ==  [[15, 4, 10, 8, 14, 8, 12, 5], 0.76]
+    :param: le nom du développeur et le nom du candidat dont on analyse le code"""
+    assert longueur_nom_variables(les_filepath(Nom_dev,Nom_Candidat)[0]) ==  [[15, 4, 10, 8, 14, 8, 12, 5], 0.76]
 
-def test_count_imbrication(emplacement_projet):
+def test_count_imbrication(Nom_dev,Nom_Candidat):
     """teste la fonction count_imbrication
-    :param: l'emplacement du projet pour completer le filepath"""
-    assert count_imbrications(emplacement_projet + "Lequipe/EventCandidatA.rb") == 0.5
+    :param: le nom du développeur et le nom du candidat dont on analyse le code"""
+    assert count_imbrications(les_filepath(Nom_dev,Nom_Candidat)[0]) == 0.5
 
-def test_nombre_duplications(emplacement_projet):
+def test_nombre_duplications(Nom_dev,Nom_Candidat):
      """teste la fonction count_duplications
-     :param: l'emplacement du projet pour completer le filepath"""
-     assert count_duplications(emplacement_projet + "Lequipe/EventCandidatA.rb") == 0
+     :param: le nom du développeur et le nom du candidat dont on analyse le code"""
+     assert count_duplications(les_filepath(Nom_dev,Nom_Candidat)[0]) == 0
 
 def test_singulier():
     """teste la fonction singulier"""
@@ -35,14 +37,14 @@ def test_proportion_True():
     assert proportion_True( ['True','False','False','True']) == 0.5
 
 
-def test_fonctions_comprehensibles(emplacement_projet):
+def test_fonctions_comprehensibles(Nom_dev,Nom_Candidat):
     """teste la fonction fonctions_comprehensibles
-    :param: l'emplacement du projet pour completer le filepath"""
-    assert type(fonctions_comprehensibles( emplacement_projet + 'Lequipe/EventCandidatA.rb')) == float
+    :param: le nom du développeur et le nom du candidat dont on analyse le code"""
+    assert type(fonctions_comprehensibles(les_filepath(Nom_dev,Nom_Candidat)[0])) == float
 
 
-def test_variables_comprehensibles(emplacement_projet):
+def test_variables_comprehensibles(Nom_dev,Nom_Candidat):
     """teste la fonction varaibles_comprehensibles
-    :param: l'emplacement du projet pour completer le filepath"""
-    assert type(variables_comprehensibles( emplacement_projet + 'Lequipe/EventCandidatA.rb')) == float
+    :param: le nom du développeur et le nom du candidat dont on analyse le code"""
+    assert type(variables_comprehensibles(les_filepath(Nom_dev,Nom_Candidat)[0])) == float
 
