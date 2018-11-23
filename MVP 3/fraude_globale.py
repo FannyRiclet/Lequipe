b289632708af2f7c30f9e2d476d9c59573bab24a
grosclara <gros.clara@hotmail.fr>
2018-11-23 13:33:16 +0100

    njl

--- MVP 3/fraude_globale.py
+++ MVP 3/fraude_globale.py
@@ -2,8 +2,7 @@
 #j'utilise aussi le tableau recapitulatif de la MVP 4
 import pandas as pd
 import sys
-import numpy as np
-
+import matplotlib.pyplot as plt
 sys.path.append('../MVP 4')
 from Tableau_recapitulatif import * #dont data_candidats
 
@@ -23,13 +22,27 @@ def comparaison_globale(Nom_dev,Nom_Candidat,liste_noms_candidats):
     :return : un tableau double entree qui, dans chaque colonne,
     donne le taux de ressemblance du code du candidat avec un autre
     candidat X, pour differents criteres (les lignes)"""
-
+    liste_noms_candidats.remove(Nom_Candidat)
     data=comparaison_deux_candidats(Nom_dev, Nom_Candidat, liste_noms_candidats[0])
     df=pd.DataFrame(data, index=[liste_noms_candidats[0]])
-    liste_noms_candidats.remove(Nom_Candidat)
     for k in range(1,len(liste_noms_candidats)) :
         data2=comparaison_deux_candidats(Nom_dev, Nom_Candidat, liste_noms_candidats[k])
         df2 = pd.DataFrame(data2, index=[liste_noms_candidats[k]])
         df=df.append(df2)
     print(df)
-    return df
+    df=df.transpose()
+    plt.style.use('ggplot')
+# figure
+    fig = plt.figure(figsize=(158, 16))
+    ax = fig.add_subplot(111)
+    ax.set_title('Pourcentage de ressemblance avec {}'.format(Nom_Candidat), color="#555555")
+# plot the differents quantities
+    for candidat in liste_noms_candidats :
+        ax.plot(df.index, df[candidat], marker="o", label=candidat, linestyle="--", linewidth=.5)
+# format and style
+    ax.set_xlabel("Critères")
+    ax.set_ylabel("Pourcentage")
+    ax.legend()
+    plt.show()
+
+comparaison_globale('Gros','CandidatA',liste_noms_candidats)
--- MVP 4/Visualisation.py
+++ MVP 4/Visualisation.py
@@ -4,14 +4,35 @@ import matplotlib.pyplot as plt
 
 def visualisation_1candidat(Nom_dev, Nom_Candidat):
     df=donnees_finales(Nom_dev,Nom_Candidat).transpose()
-    df.plot()
+    plt.style.use('ggplot')
+# figure
+    fig = plt.figure(figsize=(158, 16))
+    ax = fig.add_subplot(111)
+    ax.set_title('Résultats {}'.format(Nom_Candidat), color="#555555")
+# plot the differents quantities
+    ax.plot(df.index, df[Nom_Candidat], marker="o", label=Nom_Candidat, linestyle="--", linewidth=.5)
+# format and style
+    ax.set_xlabel("Critères")
+    ax.set_ylabel("")
+    ax.legend()
     plt.show()
 
 liste_noms_candidats=['CandidatA','CandidatB','CandidatC']
 
 def visualisation_candidats(Nom_dev,liste_noms_candidats) :
     df=data_candidats(Nom_dev, liste_noms_candidats).transpose()
-    df.plot()
+    plt.style.use('ggplot')
+# figure
+    fig = plt.figure(figsize=(158, 16))
+    ax = fig.add_subplot(111)
+    ax.set_title('Comparaison des critères entre candidats', color="#555555")
+# plot the differents quantities
+    for candidat in liste_noms_candidats :
+        ax.plot(df.index, df[candidat], marker="o", label=candidat, linestyle="--", linewidth=.5)
+# format and style
+    ax.set_xlabel("Critères")
+    ax.set_ylabel("")
+    ax.legend()
     plt.show()
 
 visualisation_1candidat('Gros','CandidatC')
